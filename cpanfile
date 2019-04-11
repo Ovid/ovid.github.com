168ce852c5e0574b2ca60a2d1efe803675196ce7
Ovid <curtis_ovid_poe@yahoo.com>
2019-04-11 08:44:27 +0200

    use Less::Boilerplate

---
+++ cpanfile
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+# vim: ft=perl
+
+=comment
+
+Install all dependencies with:
+
+    cpanm --installdeps . --with-configure --with-develop --with-all-features
+See https://metacpan.org/pod/CPAN::Meta::Spec#VERSION-NUMBERS for details.
+
+=cut
+
+requires 'Import::Into' => '1.002005';
---
+++ lib/Less/Boilerplate.pm
@@ -0,0 +1,55 @@
+package Less::Boilerplate;
+
+use 5.26.0;
+use strict;
+use warnings;
+use feature ();
+use utf8::all;
+use Carp;
+
+use Import::Into;
+
+sub import {
+    my ($class) = @_;
+
+    my $caller = caller;
+
+    warnings->import;
+    warnings->unimport('experimental::signatures');
+    strict->import;
+    feature->import(qw/signatures :5.26/);
+    utf8::all->import;
+    Carp->import::into( $caller, qw(carp croak) );
+}
+
+sub unimport {
+    warnings->unimport;
+    strict->unimport;
+    feature->unimport;
+    utf8::all->unimport;
+    Carp->unimport;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Less::Boilerplate - Use modern Perl features
+
+=head1 SYNOPSIS
+
+    use Less::Boilerplate;
+
+=head1 DESCRIPTION
+
+This module is a replacement for the following:
+
+    use strict;
+    use warnings;
+    use v5.26;
+    use feature "signatures';
+    no warnings 'experimental::signatures';
+    use utf8::all;
+    use Carp qw(carp croak);
