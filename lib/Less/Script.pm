6d6d2bb9fc3724aeb991221181228d9bb3ffc374
Ovid <curtis_ovid_poe@yahoo.com>
2019-06-13 10:57:10 +0200

    Less::Script now exports database handles.

--- lib/Less/Script.pm
+++ lib/Less/Script.pm
@@ -5,6 +5,7 @@ use Getopt::Long;
 use Import::Into;
 use Text::Unidecode;
 use String::Util 'trim';
+use DBI;
 use autodie ':all';
 use base qw(Exporter);
 
@@ -12,6 +13,7 @@ our @EXPORT = qw(
   make_slug
   splat
   slurp
+  dbh
 );
 
 sub import ( $class, %arg_for ) {
@@ -20,16 +22,40 @@ sub import ( $class, %arg_for ) {
     Getopt::Long->import::into(1);
 }
 
+=head2 C<slurp>
+
+    my $contents = slurp($filename);
+
+Read contents of C<$filename> into a variable.
+
+=cut
+
 sub slurp($filename) {
     open my $fh, '<', $filename;
     my $contents = do { local $/; <$fh> };
     return $contents;
 }
 
+=head2 C<splat>
+
+    splat($filename, $contents);
+
+Write C<$contents> to C<filename>.
+
+=cut
+
 sub splat ( $filename, $contents ) {
     open my $fh, '>', $filename;
     print {$fh} $contents;
 }
+
+=head2 C<make_slug>
+
+    my $slug = make_slug($name);
+
+Creates a URI-friendly "slug" from text.
+
+=cut
 
 sub make_slug ($name) {
 
@@ -43,3 +69,24 @@ sub make_slug ($name) {
     $name =~ s/--/-/g;
     return $name;
 }
+
+=head2 C<dbh>
+
+    my $dbh = dbh('test');
+    my $dbh = dbh('prod');
+
+Fetch a database handle to test or prod.
+
+=cut
+
+sub dbh ($db) {
+    state $dbh_for = {
+        test => DBI->connect( "dbi:SQLite:dbname=ovid_test.db", "", "" ),
+        prod => DBI->connect( "dbi:SQLite:dbname=ovidt.db",     "", "" ),
+    };
+    my $dbh = $dbh_for->{$db}
+      or croak("Could not find database handle for '$db'");
+    return $dbh;
+}
+
+1;
