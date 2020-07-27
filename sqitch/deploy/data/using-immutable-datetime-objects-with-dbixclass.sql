50e834f204b96e3b3ee4bcf9095a6971343e186a
Ovid <curtis_ovid_poe@yahoo.com>
2020-07-27 11:17:31 +0200

    Immutable Object Article

--- sqitch/deploy/data/using-immutable-datetime-objects-with-dbixclass.sql
+++ sqitch/deploy/data/using-immutable-datetime-objects-with-dbixclass.sql
@@ -4,9 +4,9 @@ BEGIN;
 
     INSERT INTO articles (title, slug, description, article_type_id, sort_order)
          VALUES (
-           'Immutable DateTime Objects with DBIx::Class',
+           'Why Do We Want Immutable Objects?',
            'using-immutable-datetime-objects-with-dbixclass',
-           'DBIx::Class is a fantastic ORM for Perl, but its standard DateTime inflation is dangerous. Let’s fix that.',
+           'I’m building a new object-oriented system to ship with the Perl language. It tries to default to immutable objects. Why is that?',
            (SELECT article_type_id FROM article_types WHERE type = 'article'),
            (SELECT max(sort_order) FROM articles) + 1 
          );
