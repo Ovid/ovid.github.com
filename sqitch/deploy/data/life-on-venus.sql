9486f90b4773879ee6b93e47d0c64f046af1b3e4
Ovid <curtis_ovid_poe@yahoo.com>
2020-09-15 11:04:40 +0200

    Fix the broken encoding in the RSS feed

--- sqitch/deploy/data/life-on-venus.sql
+++ sqitch/deploy/data/life-on-venus.sql
@@ -6,7 +6,7 @@ BEGIN;
          VALUES (
            'Life on Venus?',
            'life-on-venus',
-           'Scientists are excited about the discovery of phosphine on Venus. We don’t know of any natural prorcess which could cause this on Venus, but phosphine is regularly produced by life on Earth.',
+           'Scientists are excited about the discovery of phosphine on Venus. We don''t know of any natural prorcess which could cause this on Venus, but phosphine is regularly produced by life on Earth.',
            (SELECT article_type_id FROM article_types WHERE type = 'blog'),
            (SELECT max(sort_order) FROM articles) + 1 
          );
