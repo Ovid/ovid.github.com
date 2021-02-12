c12e8bdb000f3accad127be633357252b9220c80
Ovid <curtis_ovid_poe@yahoo.com>
2021-02-12 09:40:03 +0100

    Add table support to blogdown.

--- lib/Text/Markdown/Blog.pm
+++ lib/Text/Markdown/Blog.pm
@@ -4,6 +4,8 @@ use Less::Boilerplate;
 use parent 'Text::Markdown';
 use Const::Fast;
 
+# I should consider biting the bullet and going full multi-markdown?
+
 # this is because Text::Markdown keeps parsing text _after_ it's processed in
 # _GenerateAnchor, causing spurious HTML that we have to fix
 #
@@ -77,6 +79,233 @@ sub _GenerateAnchor (
     }
 
     return $result;
+}
+
+# _DoHeaders and _DoTables copied from
+# https://metacpan.org/release/Text-MultiMarkdown/source/lib/Text/MultiMarkdown.pm
+sub _DoHeaders ( $self, $text ) {
+    $text = $self->SUPER::_DoHeaders($text);
+
+# Do tables to populate the table id's for cross-refs
+# (but after headers as the tables can contain cross-refs to other things, so we want the header cross-refs)
+    $text = $self->_DoTables($text);
+}
+
+sub _DoTables ( $self, $text ) {
+    return $text if $self->{disable_tables};
+
+    my $less_than_tab = $self->{tab_width} - 1;
+
+    # Algorithm inspired by PHP Markdown Extra's
+    # <http://www.michelf.com/projects/php-markdown/>
+
+    # Reusable regexp's to match table
+
+    my $line_start = qr{
+        [ ]{0,$less_than_tab}
+    }mx;
+
+    my $table_row = qr{
+        [^\n]*?\|[^\n]*?\n
+    }mx;
+
+    my $first_row = qr{
+        $line_start
+        \S+.*?\|.*?\n
+    }mx;
+
+    my $table_rows = qr{
+        (\n?$table_row)
+    }mx;
+
+    my $table_caption = qr{
+        $line_start
+        \[.*?\][ \t]*\n
+    }mx;
+
+    my $table_divider = qr{
+        $line_start
+        [\|\-\:\.][ \-\|\:\.]* \| [ \-\|\:\.]*
+    }mx;
+
+    my $whole_table = qr{
+        ($table_caption)?       # Optional caption
+        ($first_row             # First line must start at beginning
+        ($table_row)*?)?        # Header Rows
+        $table_divider          # Divider/Alignment definitions
+        $table_rows+            # Body Rows
+        ($table_caption)?       # Optional caption
+    }mx;
+
+    # Find whole tables, then break them up and process them
+
+    $text =~ s{
+        ^($whole_table)         # Whole table in $1
+        (\n|\Z)                 # End of file or 2 blank lines
+    }{
+        my $table = $1;
+        my $result = "<table>\n";
+        my @alignments;
+        my $use_row_header = 0;
+ 
+        # Add Caption, if present
+ 
+        if ($table =~ s/^$line_start\[\s*(.*?)\s*\](\[\s*(.*?)\s*\])?[ \t]*$//m) {
+            if (defined $3) {
+                # add caption id to cross-ref list
+                my $table_id = $self->_Header2Label($3);
+                $result .= qq{<caption id="$table_id">} . $self->_RunSpanGamut($1). "</caption>\n";
+ 
+                $self->{_crossrefs}{$table_id} = "#$table_id";
+                $self->{_titles}{$table_id} = "$1";
+            }
+            else {
+                $result .= "<caption>" . $self->_RunSpanGamut($1). "</caption>\n";
+            }
+        }
+ 
+        # If a second "caption" is present, treat it as a summary
+        # However, this is not valid in XHTML 1.0 Strict
+        # But maybe in future
+ 
+        # A summary might be longer than one line
+        if ($table =~ s/\n$line_start\[\s*(.*?)\s*\][ \t]*\n/\n/s) {
+            # $result .= "<summary>" . $self->_RunSpanGamut($1) . "</summary>\n";
+        }
+ 
+        # Now, divide table into header, alignment, and body
+ 
+        # First, add leading \n in case there is no header
+ 
+        $table = "\n" . $table;
+ 
+        # Need to be greedy
+ 
+        $table =~ s/\n($table_divider)\n(($table_rows)+)//s;
+ 
+        my $alignment_string = $1;
+        my $body = $2;
+ 
+        # Process column alignment
+        while ($alignment_string =~ /\|?\s*(.+?)\s*(\||\Z)/gs) {
+            my $cell = $self->_RunSpanGamut($1);
+            if ($cell =~ /\:$/) {
+                if ($cell =~ /^\:/) {
+                    $result .= qq[<col align="center"$self->{empty_element_suffix}\n];
+                    push(@alignments,"center");
+                }
+                else {
+                    $result .= qq[<col align="right"$self->{empty_element_suffix}\n];
+                    push(@alignments,"right");
+                }
+            }
+            else {
+                if ($cell =~ /^\:/) {
+                    $result .= qq[<col align="left"$self->{empty_element_suffix}\n];
+                    push(@alignments,"left");
+                }
+                else {
+                    if (($cell =~ /^\./) || ($cell =~ /\.$/)) {
+                        $result .= qq[<col align="char"$self->{empty_element_suffix}\n];
+                        push(@alignments,"char");
+                    }
+                    else {
+                        $result .= "<col$self->{empty_element_suffix}\n";
+                        push(@alignments,"");
+                    }
+                }
+            }
+        }
+ 
+        # Process headers
+        $table =~ s/^\n+//s;
+ 
+        $result .= "<thead>\n";
+ 
+        # Strip blank lines
+        $table =~ s/\n[ \t]*\n/\n/g;
+ 
+        foreach my $line (split(/\n/, $table)) {
+            # process each line (row) in table
+            $result .= "<tr>\n";
+            my $count=0;
+            while ($line =~ /\|?\s*([^\|]+?)\s*(\|+|\Z)/gs) {
+                # process contents of each cell
+                my $cell = $self->_RunSpanGamut($1);
+                my $ending = $2;
+                my $colspan = "";
+                if ($ending =~ s/^\s*(\|{2,})\s*$/$1/) {
+                    $colspan = " colspan=\"" . length($ending) . "\"";
+                }
+                $result .= "\t<th$colspan>$cell</th>\n";
+                if ( $count == 0) {
+                    if ($cell =~ /^\s*$/) {
+                        $use_row_header = 1;
+                    }
+                    else {
+                        $use_row_header = 0;
+                    }
+                }
+                $count++;
+            }
+            $result .= "</tr>\n";
+        }
+ 
+        # Process body
+ 
+        $result .= "</thead>\n<tbody>\n";
+ 
+        foreach my $line (split(/\n/, $body)) {
+            # process each line (row) in table
+            if ($line =~ /^\s*$/) {
+                $result .= "</tbody>\n\n<tbody>\n";
+                next;
+            }
+            $result .= "<tr>\n";
+            my $count=0;
+            while ($line =~ /\|?\s*([^\|]+?)\s*(\|+|\Z)/gs) {
+                # process contents of each cell
+                no warnings 'uninitialized';
+                my $cell = $self->_RunSpanGamut($1);
+                my $ending = $2;
+                my $colspan = "";
+                my $cell_type = "td";
+                if ($count == 0 && $use_row_header == 1) {
+                    $cell_type = "th";
+                }
+                if ($ending =~ s/^\s*(\|{2,})\s*$/$1/) {
+                    $colspan = " colspan=\"" . length($ending) . "\"";
+                }
+                if ($alignments[$count] !~ /^\s*$/) {
+                    $result .= "\t<$cell_type$colspan align=\"$alignments[$count]\">$cell</$cell_type>\n";
+                }
+                else {
+                    $result .= "\t<$cell_type$colspan>$cell</$cell_type>\n";
+                }
+                $count++;
+            }
+            $result .= "</tr>\n";
+        }
+ 
+        $result .= "</tbody>\n</table>\n";
+        $result
+    }egmx;
+
+    my $table_body = qr{
+        (                               # wrap whole match in $2
+ 
+            (.*?\|.*?)\n                    # wrap headers in $3
+ 
+            [ ]{0,$less_than_tab}
+            ($table_divider)    # alignment in $4
+ 
+            (                           # wrap cells in $5
+                $table_rows
+            )
+        )
+    }mx;
+
+    return $text;
 }
 
 1;
--- t/blogdown.t
+++ t/blogdown.t
@@ -41,19 +41,74 @@ subtest 'basic' => sub {
     is_html $html, $expected, 'URLs with domains should open a new tab';
 };
 
+subtest 'tables' => sub {
+	# see https://fletcher.github.io/MultiMarkdown-6/syntax/tables.html
+    my $html = $blog->blogdown(<<~'END');
+    |             |          Grouping           ||
+    First Header  | Second Header | Third Header |
+     ------------ | :-----------: | -----------: |
+    Content       |          *Long Cell*        ||
+    Content       |   **Cell**    |         Cell |
+    
+    New section   |     More      |         Data |
+    And more      | With an escaped '\|'         ||
+    [Prototype table]
+    END
+    my $expected = <<~'END';
+    <table>
+    <caption>Prototype table</caption>
+    <col />
+    <col align="center" />
+    <col align="right" />
+    <thead>
+    <tr>
+    	<th> </th>
+    	<th colspan="2">Grouping</th>
+    </tr>
+    <tr>
+    	<th>First Header</th>
+    	<th>Second Header</th>
+    	<th>Third Header</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+    	<td>Content</td>
+    	<td colspan="2" align="center"><em>Long Cell</em></td>
+    </tr>
+    <tr>
+    	<td>Content</td>
+    	<td align="center"><strong>Cell</strong></td>
+    	<td align="right">Cell</td>
+    </tr>
+    </tbody>
+    
+    <tbody>
+    <tr>
+    	<td>New section</td>
+    	<td align="center">More</td>
+    	<td align="right">Data</td>
+    </tr>
+    <tr>
+    	<td>And more</td>
+    	<td align="center">With an escaped '\</td>
+    	<td colspan="2" align="right">'</td>
+    </tr>
+    </tbody>
+    </table>
+    END
+    is_html $html, $expected, 'We should be able to build rich tables';
+};
+
 done_testing;
 
 sub is_html ( $have, $want, $message ) {
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    my @have = map { rtrim($_) } split /\n/ => trim($have);
-    my @want = map { rtrim($_) } split /\n/ => trim($want);
+    my @have = map { trim($_) } split /\n/ => trim($have);
+    my @want = map { trim($_) } split /\n/ => trim($want);
     eq_or_diff \@have, \@want, $message;
 }
 
 sub trim ($text) {
     $text =~ s/^\s+|\s+$//gr;
-}
-
-sub rtrim ($text) {
-    $text =~ s/\s+$//r;
 }
