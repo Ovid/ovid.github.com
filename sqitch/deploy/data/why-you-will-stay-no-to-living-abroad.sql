b2248e60433ea71a70b9cc3b3ed2931ef00459da
Ovid <ovid@cpan.org>
2022-06-29 18:08:18 +0200

    Fix blog title (oops!). Thanks, Gizmo Mathboy!

--- sqitch/deploy/data/why-you-will-stay-no-to-living-abroad.sql
+++ sqitch/deploy/data/why-you-will-stay-no-to-living-abroad.sql
@@ -4,8 +4,8 @@ BEGIN;
 
     INSERT INTO articles (title, slug, description, article_type_id, sort_order)
          VALUES (
-           'Why You Will Stay "No" to Living Abroad',
-           'why-you-will-stay-no-to-living-abroad',
+           'Why You Will Say "No" to Living Abroad',
+           'why-you-will-say-no-to-living-abroad',
            'Many people fantasize about living abroad. However, if they''re given the chance, they might say no. Here''s why.',
            (SELECT article_type_id FROM article_types WHERE type = 'blog'),
            (SELECT max(sort_order) FROM articles) + 1 
--- sqitch/revert/data/why-you-will-stay-no-to-living-abroad.sql
+++ sqitch/revert/data/why-you-will-stay-no-to-living-abroad.sql
@@ -2,6 +2,6 @@
 
 BEGIN;
 
-    DELETE FROM articles WHERE slug = 'why-you-will-stay-no-to-living-abroad' AND article_type_id = (SELECT article_type_id FROM article_types WHERE type = 'blog');
+    DELETE FROM articles WHERE slug = 'why-you-will-say-no-to-living-abroad' AND article_type_id = (SELECT article_type_id FROM article_types WHERE type = 'blog');
 
 COMMIT;
