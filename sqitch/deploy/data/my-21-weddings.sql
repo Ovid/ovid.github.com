dff273d5d3775fa8d66b0624cced9697a8a5c35b
Ovid <ovid@cpan.org>
2022-11-21 15:53:17 +0100

    Oops. Rewrite the description. Bad math :)

--- sqitch/deploy/data/my-21-weddings.sql
+++ sqitch/deploy/data/my-21-weddings.sql
@@ -6,7 +6,7 @@ BEGIN;
          VALUES (
            'My 21 Weddings',
            'my-21-weddings',
-           'I have participated in 21 weddings. Twice as the groom, 17 times as the minister. It''s been a wild ride.',
+           'I have participated in 21 weddings. Twice as the groom, 19 times as the minister. It''s been a wild ride.',
            (SELECT article_type_id FROM article_types WHERE type = 'blog'),
            (SELECT max(sort_order) FROM articles) + 1 
          );
