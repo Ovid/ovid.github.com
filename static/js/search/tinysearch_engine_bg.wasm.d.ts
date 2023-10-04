893d144d02585a27265c9032288939097b1f9f5a
Ovid <ovid@cpan.org>
2023-10-04 09:15:53 +0200

    Add our new search engine support

---
+++ static/js/search/tinysearch_engine_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function search(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
