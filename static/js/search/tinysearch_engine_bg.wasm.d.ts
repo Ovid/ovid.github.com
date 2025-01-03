86c02b423336077c1b49be98c3e5b0302f441103
Ovid <curtis.poe@gmail.com>
2025-01-03 10:44:45 +0100

    Updsate the search engine to use the new API

--- static/js/search/tinysearch_engine_bg.wasm.d.ts
+++ static/js/search/tinysearch_engine_bg.wasm.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const search: (a: number, b: number, c: number) => number;
-export const __wbindgen_malloc: (a: number, b: number) => number;
-export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export function search(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
