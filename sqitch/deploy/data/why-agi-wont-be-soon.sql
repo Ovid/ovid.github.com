ffad433fbf50cb06ffde22cbaff74108108c9c95
Ovid <curtis.poe@gmail.com>
2025-01-04 13:35:57 +0100

    Update AGI article titles to be title case

--- sqitch/deploy/data/why-agi-wont-be-soon.sql
+++ sqitch/deploy/data/why-agi-wont-be-soon.sql
@@ -4,7 +4,7 @@ BEGIN;
 
     INSERT INTO articles (title, slug, description, article_type_id, sort_order)
          VALUES (
-           'Why AGI won''t be soon',
+           'Why AGI Won''t Be Soon',
            'why-agi-wont-be-soon',
            'There''s been a lot of talk about AGI coming soon, but there are some strong reasons to think this isn''t true.',
            (SELECT article_type_id FROM article_types WHERE type = 'article'),
