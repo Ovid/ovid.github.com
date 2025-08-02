773b9c4a2b97e8fffa34c8721bf5d11a127c61e0
Ovid <curtis.poe@gmail.com>
2025-08-02 07:54:25 +0200

    Fix UTF-8 corruption in title. Need longer-term fix

--- sqitch/deploy/data/a-geodetic-refutation-of-flat-earth-theory.sql
+++ sqitch/deploy/data/a-geodetic-refutation-of-flat-earth-theory.sql
@@ -6,7 +6,7 @@ BEGIN;
          VALUES (
            'A Geodetic Refutation of Flat-Earth Theory',
            'a-geodetic-refutation-of-flat-earth-theory',
-           'A Formal Refutation of âFlatâEarthâ Claims through the Lens of NegativeâCurvature Geodesy',
+           'A Formal Refutation of "Flat Earth" Claims through the Lens of Negative-Curvature Geodesy',
            (SELECT article_type_id FROM article_types WHERE type = 'blog'),
            (SELECT max(sort_order) FROM articles) + 1 
          );
